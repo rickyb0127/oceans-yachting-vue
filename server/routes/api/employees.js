0c960dcab2368134884d5b061d4bab7fcc7b42eb
eric bartsch <bartsched@gmail.com>
2019-06-24 08:07:50 -0400

    add edit-company-profile

--- server/routes/api/employees.js
+++ server/routes/api/employees.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const router = express.Router();
 const Schema = mongoose.Schema;
 const multer = require('multer');
+let middleware = require('../../middleware');
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
@@ -46,9 +47,9 @@ router.get("/", (req, res) => {
 
 //POST
 router.post("/", upload.single('photo'), (req, res) => {
-  // const re = /,\s/;
   let bulletPointsArray = req.body.bullets.split(",");
-  let photoString = req.body.first_name.toLowerCase() + '-' + req.body.last_name.toLowerCase() + '.jpg';
+  let photoString = "generic-profile-picture.jpg";
+  // let photoString = req.body.first_name.toLowerCase() + '-' + req.body.last_name.toLowerCase() + '.jpg';
 
   var new_employee = new Employee({
     first_name: req.body.first_name,
@@ -72,9 +73,66 @@ router.post("/", upload.single('photo'), (req, res) => {
   });
 });
 
+//EDIT
+router.get("/:id", function (req, res) {
+  const id = req.params.id;
+  Employee.findById(id, function (err, employee){
+    res.send({
+      employee
+    });
+  });
+});
+
+//UPDATE 
+router.put("/:id", middleware.checkToken, function(req, res) {
+  Employee.findOne({ _id: req.params.id }, function (error, employee) {
+    if(error) {
+      return res.status(500).send('Error on the server.');
+    } 
+    if(!employee) {
+      return res.status(404).send('No employee found.');
+    } 
+
+    if(req.body.first_name) {
+      employee.first_name = req.body.first_name;
+    }
+    if(req.body.last_name) {
+      employee.last_name = req.body.last_name;
+    }
+    if(req.body.title) {
+      employee.title = req.body.title;
+    }
+    if(req.body.bio) {
+      employee.bio = req.body.bio;
+    }
+    if(req.body.quote) {
+      employee.quote = req.body.quote;
+    }
+    if(req.body.bullet_points) {
+      let bulletPointsArray = req.body.bullet_points.split(",");
+      employee.bullet_points = bulletPointsArray;
+    }
+    if(req.body.photo_string) {
+      // let photoString = req.body.first_name.toLowerCase() + '-' + req.body.last_name.toLowerCase() + '.jpg';
+      employee.photo_name = req.body.photo_string;
+    }
+
+    employee.save(function (error) {
+      if (error) {
+        console.log(error);
+      } else {
+        res.send({
+          success: true,
+          message: "Employee updated successfully"
+        });
+      }
+    });
+  });
+});
+
 //DELETE
-router.delete("/:id", (req, res) => {
-  User.deleteOne({
+router.delete("/:id", middleware.checkToken, (req, res) => {
+  Employee.deleteOne({
     _id: req.params.id
   }, function (error, employee) {
     if (error) {
